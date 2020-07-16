ef1fadf64584fc67f90b903ad52d0f46bc4f8063
BeokBeok <kekemusa37@nate.com>
2020-07-16 14:32:27 +0900

    [적용] AndroidChannelBuilder 및 CoroutineStub

--- app/src/main/java/com/example/grpc_android/MainActivity.kt
+++ app/src/main/java/com/example/grpc_android/MainActivity.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.protobuf.Timestamp
 import io.grpc.ManagedChannel
-import io.grpc.ManagedChannelBuilder
 import io.grpc.Metadata
-import io.grpc.chat.ChatGrpc
+import io.grpc.android.AndroidChannelBuilder
+import io.grpc.chat.ChatGrpcKt
 import io.grpc.chat.OpenStream
 import io.grpc.chat.Payload
 import io.grpc.chat.Request
@@ -15,35 +15,34 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
-    private val chatChannel: ManagedChannel =
-        ManagedChannelBuilder.forAddress("qa-chat.conects.com", 10620)
-            .usePlaintext()
+    private val chatChannel: ManagedChannel by lazy {
+        AndroidChannelBuilder.forAddress("qa-chat.conects.com", 10620)
+            .context(applicationContext)
             .executor(Executors.newSingleThreadExecutor().asCoroutineDispatcher().executor)
             .build()
+    }
 
-    private val chatBlockingStub: ChatGrpc.ChatBlockingStub = MetadataUtils.attachHeaders(
-        ChatGrpc.newBlockingStub(chatChannel),
-        Metadata().apply {
-            put(
-                Metadata.Key.of("Authorization", Metadata.ASCII_STRING_MARSHALLER),
-                "Bearer test"
-            )
-        }
-    )
+    private val chatBlockingStub: ChatGrpcKt.ChatCoroutineStub by lazy {
+        MetadataUtils.attachHeaders(
+            ChatGrpcKt.ChatCoroutineStub(chatChannel),
+            Metadata().apply {
+                put(
+                    Metadata.Key.of("Authorization", Metadata.ASCII_STRING_MARSHALLER),
+                    "Bearer yunsu"
+                )
+            }
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        setupListener()
-    }
-
-    private fun setupListener() {
         setupEventListen()
     }
 
@@ -52,7 +51,7 @@ class MainActivity : AppCompatActivity() {
             CoroutineScope(Dispatchers.IO).launch {
                 val payloadModel = Payload.newBuilder().apply {
                     stream = OpenStream.newBuilder().apply {
-                        name = "test"
+                        name = "yunsu"
                     }.build()
                 }.build()
 
@@ -61,7 +60,7 @@ class MainActivity : AppCompatActivity() {
                     payload = payloadModel
                 }.build()
 
-                chatBlockingStub.eventListen(request).forEach { receive ->
+                chatBlockingStub.eventListen(request).collect { receive ->
                     println("$receive")
                 }
             }
