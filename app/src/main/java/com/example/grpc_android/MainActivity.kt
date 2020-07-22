4d58c80095f9ab41f31e31e7cd160730a88ab537
BeokBeok <kekemusa37@nate.com>
2020-07-22 10:57:56 +0900

    [적용] 데이터바인딩

--- app/src/main/java/com/example/grpc_android/MainActivity.kt
+++ app/src/main/java/com/example/grpc_android/MainActivity.kt
@@ -3,10 +3,11 @@ package com.example.grpc_android
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.grpc_android.databinding.ActivityMainBinding
 import dagger.android.support.DaggerAppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 class MainActivity : DaggerAppCompatActivity() {
@@ -15,76 +16,21 @@ class MainActivity : DaggerAppCompatActivity() {
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
     private val viewModel by viewModels<MainViewModel> { viewModelFactory }
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        setupEventListen()
-        setupChatWith()
-        setupChatIn()
-        setupChatOut()
-        setupSendMessage()
-        setupGetMessage()
+        setupBinding()
         setupObserve()
     }
 
-    /**
-     * 채팅 관련 각종 이벤트를 받을 수 있는 리스너 등록
-     */
-    private fun setupEventListen() {
-        btn_event_listen.setOnClickListener {
-            viewModel.eventListen(tiet_uid.text.toString())
-        }
-    }
-
-    /**
-     * 채팅방 생성
-     */
-    private fun setupChatWith() {
-        btn_chat_with.setOnClickListener {
-            viewModel.chatWith(tiet_uid.text.toString(), "test1")
-        }
-    }
-
-    /**
-     * 채팅방 진입
-     */
-    private fun setupChatIn() {
-        btn_chat_in.setOnClickListener {
-            viewModel.chatIn(tiet_uid.text.toString(), tiet_cid.text.toString())
-        }
-    }
-
-    /**
-     * 채팅방 퇴장
-     */
-    private fun setupChatOut() {
-        btn_chat_out.setOnClickListener {
-            viewModel.chatOut(tiet_uid.text.toString(), tiet_cid.text.toString())
-        }
-    }
-
-    /**
-     * 메시지 전송
-     */
-    private fun setupSendMessage() {
-        btn_send_message.setOnClickListener {
-            viewModel.sendMessage(
-                tiet_uid.text.toString(),
-                tiet_cid.text.toString(),
-                tiet_message.text.toString()
-            )
-        }
-    }
-
-    /**
-     * 채팅방 메시지 조회
-     */
-    private fun setupGetMessage() {
-        btn_get_message.setOnClickListener {
-            viewModel.getMessages(tiet_cid.text.toString())
-        }
+    private fun setupBinding() {
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+            .apply {
+                lifecycleOwner = this@MainActivity
+                vm = viewModel
+            }
     }
 
     private fun setupObserve() {
@@ -96,7 +42,7 @@ class MainActivity : DaggerAppCompatActivity() {
             })
             cid.observe(owner, Observer {
                 println("cid is $it")
-                tiet_cid.setText(it)
+                binding.tietCid.setText(it)
             })
             errMsg.observe(owner, Observer {
                 Toast.makeText(owner, it, Toast.LENGTH_SHORT).show()
