a2cf764faa637f5df8a9153fc390c742ae1b5ee1
BeokBeok <kekemusa37@nate.com>
2020-07-23 17:06:16 +0900

    [변경] 변수명 (lastChatId -> lid)

--- app/src/main/java/com/example/grpc_android/talk/model/MessageData.kt
+++ app/src/main/java/com/example/grpc_android/talk/model/MessageData.kt
@@ -3,14 +3,14 @@ package com.example.grpc_android.talk.model
 import io.grpc.chat.Message
 
 data class MessageData(
-    val lastChatId: String,
+    val lid: String,
     val message: String,
     val transactionId: String,
     val userId: String
 )
 
 fun Message.mapToPresenter() = MessageData(
-    lastChatId = this.lid,
+    lid = this.lid,
     message = this.message,
     transactionId = this.tid,
     userId = this.uid
