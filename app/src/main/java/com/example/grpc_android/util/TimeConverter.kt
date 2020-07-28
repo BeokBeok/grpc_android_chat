97fa5376bd43420ac3e6173455b0085a5e6ada4e
BeokBeok <kekemusa37@nate.com>
2020-07-28 14:00:45 +0900

    [적용] 타임스탬프 마지막 3자리를 0으로 변경
    - 마지막 3자리가 milliseconds 라서 클라이언트에서는 초단위가 필요 없음

--- app/src/main/java/com/example/grpc_android/util/TimeConverter.kt
+++ app/src/main/java/com/example/grpc_android/util/TimeConverter.kt
@@ -7,19 +7,21 @@ import java.util.*
 object TimeConverter {
 
     private const val A_H_MM = "a h:mm"
+    private const val YYYY_MM_DD_EEEE = "yyyy-MM-dd EEEE"
 
     @SuppressLint("SimpleDateFormat")
     private val ahmmDateFormat = SimpleDateFormat(A_H_MM)
 
-    private const val YYYY_MM_DD_EEEE = "yyyy-MM-dd EEEE"
-
     @SuppressLint("SimpleDateFormat")
     private val yyyymmddeeeDateFormat = SimpleDateFormat(YYYY_MM_DD_EEEE)
 
+    private val TIMESTAMP_RANGE = 1..16
 
-    fun lidToHourMinute(lid: String): String =
-        ahmmDateFormat.format(Date(lid.substring(1..16).toLong(16) * 1_000))
+    fun lidToHourMinute(lid: String): String = ahmmDateFormat.format(Date(lidToTimestamp(lid)))
 
     fun lidToYearMonthDay(lid: String): String =
-        yyyymmddeeeDateFormat.format(Date(lid.substring(1..16).toLong(16) * 1_000))
+        yyyymmddeeeDateFormat.format(Date(lidToTimestamp(lid)))
+
+    private fun lidToTimestamp(lid: String) =
+        lid.substring(TIMESTAMP_RANGE).toLong(16) / 1_000 * 1_000
 }
