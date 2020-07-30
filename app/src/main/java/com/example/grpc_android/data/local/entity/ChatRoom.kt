34f84cb5d48edb694e7d69b4d5a112ce60875477
BeokBeok <kekemusa37@nate.com>
2020-07-30 15:12:03 +0900

    [추가] ChatRoom 엔티티

---
+++ app/src/main/java/com/example/grpc_android/data/local/entity/ChatRoom.kt
@@ -0,0 +1,10 @@
+package com.example.grpc_android.data.local.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "ChatRoom")
+data class ChatRoom(
+    @PrimaryKey @ColumnInfo(name = "cid") val chatId: String
+)
