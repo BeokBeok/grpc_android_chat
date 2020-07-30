477d609d84a17032f7db203de002997af82be141
BeokBeok <kekemusa37@nate.com>
2020-07-30 16:51:53 +0900

    [적용] BaseDao

--- app/src/main/java/com/example/grpc_android/data/local/ChatRoomDao.kt
+++ app/src/main/java/com/example/grpc_android/data/local/ChatRoomDao.kt
@@ -1,16 +1,12 @@
 package com.example.grpc_android.data.local
 
 import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.example.grpc_android.base.BaseDao
 import com.example.grpc_android.data.local.entity.ChatRoom
 
 @Dao
-interface ChatRoomDao {
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertChatRoom(chatRoom: ChatRoom)
+interface ChatRoomDao : BaseDao<ChatRoom> {
 
     @Query("SELECT * FROM ChatRoom")
     suspend fun getChatRoom(): List<ChatRoom>
