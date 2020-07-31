e1986891849fddb2b26e7aa7e49745af4df2622d
BeokBeok <kekemusa37@nate.com>
2020-07-31 17:45:24 +0900

    [정리] ChatDataRepository 코드

--- app/src/main/java/com/example/grpc_android/data/ChatDataRepository.kt
+++ app/src/main/java/com/example/grpc_android/data/ChatDataRepository.kt
@@ -19,81 +19,87 @@ class ChatDataRepository @Inject constructor(
     private val ioDispatcher = Dispatchers.IO
 
     override fun eventListen(uid: String): Flow<Receive> {
-        val request = EventListenRequest.newBuilder().apply {
-            meta = Meta.newBuilder().apply {
-                this.uid = uid
-                timestamp = Timestamp.getDefaultInstance()
-            }.build()
-        }.build()
+        val meta = Meta.newBuilder()
+            .setUid(uid)
+            .setTimestamp(Timestamp.getDefaultInstance())
+            .build()
+        val request = EventListenRequest.newBuilder()
+            .setMeta(meta)
+            .build()
 
         return chatRemoteDataSource.eventListen(request).flowOn(ioDispatcher)
     }
 
     override suspend fun chatWith(uid: String, peerName: String): Result<CreateResponse> =
         withContext(ioDispatcher) {
-            val request = CreateRequest.newBuilder().apply {
-                meta = Meta.newBuilder().apply {
-                    this.uid = uid
-                    peer = peerName
-                }.build()
-            }.build()
+            val meta = Meta.newBuilder()
+                .setUid(uid)
+                .build()
+            val request = CreateRequest.newBuilder()
+                .setMeta(meta)
+                .build()
 
             runCatching { chatRemoteDataSource.chatWith(request) }
         }
 
     override suspend fun sendMessage(uid: String, cid: String, msg: String): Result<WriteResponse> =
         withContext(ioDispatcher) {
-            val request = WriteRequest.newBuilder().apply {
-                meta = Meta.newBuilder().apply {
-                    this.uid = uid
-                    this.cid = cid
-                    message = msg
-                }.build()
-            }.build()
+            val meta = Meta.newBuilder()
+                .setUid(uid)
+                .setCid(cid)
+                .build()
+            val request = WriteRequest.newBuilder()
+                .setMeta(meta)
+                .setMessage(msg)
+                .build()
 
             runCatching { chatRemoteDataSource.sendMessage(request) }
         }
 
     override suspend fun chatIn(uid: String, cid: String): Result<ChatInResponse> =
         withContext(ioDispatcher) {
-            val request = ChatInRequest.newBuilder().apply {
-                meta = Meta.newBuilder().apply {
-                    this.uid = uid
-                    this.cid = cid
-                    chatPublicChecksum = "00000000000000000"
-                    chatInChecksum = "00000000000000000"
-                }.build()
-            }.build()
+            val meta = Meta.newBuilder()
+                .setUid(uid)
+                .setCid(cid)
+                .build()
+            val request = ChatInRequest.newBuilder()
+                .setMeta(meta)
+                .setChatPublicChecksum("00000000000000000")
+                .setChatInChecksum("00000000000000000")
+                .build()
 
             runCatching { chatRemoteDataSource.chatIn(request) }
         }
 
     override suspend fun chatOut(uid: String, cid: String): Result<ChatOutResponse> =
         withContext(ioDispatcher) {
-            val request = ChatOutRequest.newBuilder().apply {
-                meta = Meta.newBuilder().apply {
-                    this.uid = uid
-                    this.cid = cid
-                    lastMsgLid = "0"
-                }.build()
-            }.build()
+            val meta = Meta.newBuilder()
+                .setUid(uid)
+                .setCid(cid)
+                .build()
+            val request = ChatOutRequest.newBuilder()
+                .setMeta(meta)
+                .setLastMsgLid("0")
+                .build()
 
             runCatching { chatRemoteDataSource.chatOut(request) }
         }
 
     override suspend fun getMessages(uid: String, cid: String): Result<GetMessagesResponse> =
         withContext(ioDispatcher) {
-            val request = GetMessagesRequest.newBuilder().apply {
-                meta = Meta.newBuilder().apply {
-                    lastLid = "0"
-                    this.uid = uid
-                    this.cid = cid
-                }.build()
-                pagination = Pagination.newBuilder().apply {
-                    pageSize = 1_000
-                    pageToken = 1
-                }.build()
+            val meta = Meta.newBuilder()
+                .setUid(uid)
+                .setCid(cid)
+                .build()
+            val pagination = Pagination.newBuilder().apply {
+                pageSize = 1_000
+                pageToken = 1
             }.build()
+            val request = GetMessagesRequest.newBuilder()
+                .setMeta(meta)
+                .setLastLid("0")
+                .setPagination(pagination)
+                .build()
 
             runCatching { chatRemoteDataSource.getMessages(request) }
         }
@@ -107,33 +113,29 @@ class ChatDataRepository @Inject constructor(
         withContext(ioDispatcher) {
             runCatching {
                 val cachedChatRooms = chatLocalDataSource.getChatRooms()
-                var request = SyncChatsRequest.newBuilder().apply {
-                    meta = Meta.newBuilder().apply {
-                        this.uid = uid
-                        lastCid = "0"
-                        syncChatChecksum = "0"
-                        fetchCount = SYNC_CHAT_FETCH_COUNT
-                    }.build()
-                }.build()
-                var fetchedChatRooms = chatRemoteDataSource.syncChats(request)
-                if (fetchedChatRooms.eof) {
-                    chatLocalDataSource.saveChatRoom(
-                        *fetchedChatRooms.updCidsList.map(::ChatRoom).toTypedArray()
-                    )
-                    return@runCatching chatLocalDataSource.getChatRooms()
-                } else {
+                if (cachedChatRooms.isEmpty()) {
+                    val meta = Meta.newBuilder()
+                        .setUid(uid)
+                        .build()
+                    var request = SyncChatsRequest.newBuilder()
+                        .setMeta(meta)
+                        .setLastCid("0")
+                        .setSyncChatChecksum("0")
+                        .setFetchCount(SYNC_CHAT_FETCH_COUNT)
+                        .build()
+                    var fetchedChatRooms = chatRemoteDataSource.syncChats(request)
+                    if (fetchedChatRooms.updCidsCount == 0) return@runCatching emptyList<ChatRoom>()
+
                     while (!fetchedChatRooms.eof) {
                         chatLocalDataSource.saveChatRoom(
                             *fetchedChatRooms.updCidsList.map(::ChatRoom).toTypedArray()
                         )
-                        request = SyncChatsRequest.newBuilder().apply {
-                            meta = Meta.newBuilder().apply {
-                                this.uid = uid
-                                lastCid = fetchedChatRooms.lastCid
-                                syncChatChecksum = fetchedChatRooms.syncChatChecksum
-                                fetchCount = SYNC_CHAT_FETCH_COUNT
-                            }.build()
-                        }.build()
+                        request = SyncChatsRequest.newBuilder()
+                            .setMeta(meta)
+                            .setLastCid(fetchedChatRooms.lastCid)
+                            .setSyncChatChecksum(fetchedChatRooms.syncChatChecksum)
+                            .setFetchCount(SYNC_CHAT_FETCH_COUNT)
+                            .build()
                         fetchedChatRooms = chatRemoteDataSource.syncChats(request)
                     }
                     chatLocalDataSource.saveChatRoom(
@@ -141,6 +143,7 @@ class ChatDataRepository @Inject constructor(
                     )
                     return@runCatching chatLocalDataSource.getChatRooms()
                 }
+                return@runCatching emptyList<ChatRoom>()
             }
         }
 
