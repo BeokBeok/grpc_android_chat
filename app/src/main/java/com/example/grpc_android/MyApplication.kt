2519924546a1099bdcb4c9a17fb7ce0fd8740590
BeokBeok <kekemusa37@nate.com>
2020-07-31 12:26:39 +0900

    [연동] flipper

--- app/src/main/java/com/example/grpc_android/MyApplication.kt
+++ app/src/main/java/com/example/grpc_android/MyApplication.kt
@@ -1,6 +1,12 @@
 package com.example.grpc_android
 
 import com.example.grpc_android.di.DaggerAppComponent
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.sharedpreferences.SharedPreferencesFlipperPlugin
+import com.facebook.soloader.SoLoader
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 import org.conscrypt.Conscrypt
@@ -11,8 +17,20 @@ class MyApplication : DaggerApplication() {
     override fun onCreate() {
         super.onCreate()
         Security.insertProviderAt(Conscrypt.newProvider(), 1)
+        setupFlipper()
     }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
         DaggerAppComponent.builder().application(this).build()
+
+    private fun setupFlipper() {
+        if (BuildConfig.DEBUG) {
+            SoLoader.init(this, false)
+            AndroidFlipperClient.getInstance(this).also {
+                it.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
+                it.addPlugin(DatabasesFlipperPlugin(this))
+                it.addPlugin(SharedPreferencesFlipperPlugin(this))
+            }.start()
+        }
+    }
 }
