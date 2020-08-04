5fd63d9d52006c0e9797f9ac7fb04d16f7cf8c3e
BeokBeok <kekemusa37@nate.com>
2020-08-04 11:52:28 +0900

    [추가] update 및 delete 쿼리

--- app/src/main/java/com/example/grpc_android/base/BaseDao.kt
+++ app/src/main/java/com/example/grpc_android/base/BaseDao.kt
@@ -1,9 +1,17 @@
 package com.example.grpc_android.base
 
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Update
 
 interface BaseDao<T> {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(vararg obj: T)
+
+    @Update
+    fun update(obj: T)
+
+    @Delete
+    fun delete(obj: T)
 }
