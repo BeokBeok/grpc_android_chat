1c7529fafdbadaffb51a54d4075a4002e02e1723
BeokBeok <kekemusa37@nate.com>
2020-08-06 16:17:18 +0900

    [추가] ChetMessage Entity

---
+++ app/src/main/java/com/example/grpc_android/data/entity/ChatMessage.kt
@@ -0,0 +1,24 @@
+package com.example.grpc_android.data.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "ChatMessage",
+    foreignKeys = [
+        ForeignKey(
+            entity = ChatRoom::class,
+            parentColumns = arrayOf("cid"),
+            childColumns = arrayOf("cid"),
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
+data class ChatMessage(
+    @PrimaryKey @ColumnInfo(name = "cid") val chatId: String,
+
+    @ColumnInfo(name = "messages")
+    val messages: List<String>
+)
